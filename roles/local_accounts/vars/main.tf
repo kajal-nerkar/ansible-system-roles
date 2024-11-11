566c4a173c80bcbacc8f44a1dd683dcce9ad95a7
kajal-nerkar <140372480+kajal-nerkar@users.noreply.github.com>
2024-11-11 16:32:36 +0200

    Create main.tf

---
+++ roles/local_accounts/vars/main.tf
@@ -0,0 +1,15 @@
+local_accounts_users:
+  - name: local_adm
+    shell: /bin/bash
+    uid: 38000087
+    expiry_date: ''
+    home: /home/local_adm
+    groups: []
+    ssh_public_key: ''  # Add public key content here if needed
+  - name: local_log
+    shell: /bin/sh
+    uid: 38000088
+    expiry_date: '2024-12-31'
+    home: /home/local_log
+    groups: []
+    ssh_public_key: ''  # Add public key content here if needed
